3df3f421f2f8059b06f8986eaa0077ae1bd0961a
Rob Kelder <dev@kelderro.nl>
2022-12-09 15:54:06 +0100

    Initialize project

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿var dayInput = ReadInput(1);
+Console.WriteLine(DayOne.PartOne(dayInput));
+Console.WriteLine(DayOne.PartTwo(dayInput));
+
+string[] ReadInput(int dayNumber)
+{
+    return File.ReadAllLines($"./day-{dayNumber}/input.txt");
+}
