e6f272784ba3099b50ec95ba27b8f81bf0e79893
Rob Kelder <dev@kelderro.nl>
2022-12-09 16:31:50 +0100

    Removing day one code from program

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿var dayInput = ReadInput(1);
-Console.WriteLine(DayOne.PartOne(dayInput));
-Console.WriteLine(DayOne.PartTwo(dayInput));
-
-string[] ReadInput(int dayNumber)
+﻿string[] ReadInput(int dayNumber)
 {
     return File.ReadAllLines($"./day-{dayNumber}/input.txt");
 }
