0a3d870020c841c5875416dd5793e6da57e5921c
Rob op den Kelder <dev@kelderro.nl>
2022-12-10 17:09:47 +0100

    Completed day-4 challenge (#6)

---
+++ day-4/DayFourTests.cs
@@ -0,0 +1,39 @@
+using Xunit;
+using FluentAssertions;
+
+public class DayFourTests
+{
+    [Fact]
+    public void PartOne_OnExampleTestCase_ReturnNumberOfOverlappingOccurences()
+    {
+        var input = """
+        2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+        """.Split(Environment.NewLine);
+
+        var result = DayFour.PartOne(input);
+
+        result.Should().Be(2);
+    }
+
+    [Fact]
+    public void PartTwo_OnExampleTestCase_ReturnNumberOfOverlappingSections()
+    {
+        var input = """
+        2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+        """.Split(Environment.NewLine);
+
+        var result = DayFour.PartTwo(input);
+
+        result.Should().Be(4);
+    }
+}
