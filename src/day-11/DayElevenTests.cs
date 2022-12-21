e09c09f0bb1551e2a28d8a03d6b99f40844dd7d6
Rob op den Kelder <dev@kelderro.nl>
2022-12-21 17:51:09 +0100

    Solve day-11 puzzle (#18)
    * Solve day-11 puzzle

---
+++ src/day-11/DayElevenTests.cs
@@ -0,0 +1,76 @@
+using FluentAssertions;
+using Xunit;
+
+public class DayEleventTests
+{
+    private string[] unitTestInput = """
+    Monkey 0:
+      Starting items: 79, 98
+      Operation: new = old * 19
+      Test: divisible by 23
+        If true: throw to monkey 2
+        If false: throw to monkey 3
+
+    Monkey 1:
+      Starting items: 54, 65, 75, 74
+      Operation: new = old + 6
+      Test: divisible by 19
+        If true: throw to monkey 2
+        If false: throw to monkey 0
+
+    Monkey 2:
+      Starting items: 79, 60, 97
+      Operation: new = old * old
+      Test: divisible by 13
+        If true: throw to monkey 1
+        If false: throw to monkey 3
+
+    Monkey 3:
+      Starting items: 74
+      Operation: new = old + 3
+      Test: divisible by 17
+        If true: throw to monkey 0
+        If false: throw to monkey 1
+    """.Split(Environment.NewLine);
+
+    [Fact]
+    public void PartOne_OnExampleTestCase_ReturnMonkeyBusinessAmount()
+    {
+        var result = DayElevent.PartOne(this.unitTestInput);
+
+        result.Should().Be(10605);
+    }
+
+    [Fact]
+    public void PartOne_OnUsingInputFile_ReturnMonkeyBusinessAmount()
+    {
+        var fileInput = ReadInput(11);
+
+        var result = DayElevent.PartOne(fileInput);
+
+        result.Should().Be(62491);
+    }
+
+    [Fact]
+    public void PartTwo_OnExampleTestCase_ReturnMonkeyBusinessAmount()
+    {
+        var result = DayElevent.PartTwo(this.unitTestInput);
+
+        result.Should().Be(2713310158);
+    }
+
+    [Fact]
+    public void PartTwo_OnUsingInputFile_ReturnMonkeyBusinessAmount()
+    {
+        var fileInput = ReadInput(11);
+
+        var result = DayElevent.PartTwo(fileInput);
+
+        result.Should().Be(17408399184);
+    }
+
+    string[] ReadInput(int dayNumber)
+    {
+        return File.ReadAllLines($"./src/day-{dayNumber}/input.txt");
+    }
+}
