e7d8e0e106b0dbd667b5dbd40327b723eec3dcf2
Rob Kelder <dev@kelderro.nl>
2022-12-21 17:45:35 +0100

    Optimizing the code of day-11 solution

--- src/day-11/DayEleven.cs
+++ src/day-11/DayEleven.cs
@@ -5,85 +5,135 @@ using System.Text;
 /// </summary>
 public abstract class DayElevent : IDay<long>
 {
-    #region Part One
     /// <summary>
     /// Figure out which monkeys to chase by counting how many items they
     /// inspect over 20 rounds. What is the level of monkey business after
     /// 20 rounds of stuff-slinging simian shenanigans?
     /// </summary>
     /// <param name="lines">Instructions</param>
-    /// <returns>Level of monkey business after 20 rounds</returns>
+    /// <returns>Level of monkey business after 20 rounds.</returns>
     public static long PartOne(string[] lines)
     {
-        var monkeyBusiness = Calculate(lines, 20, 3);
+        var monkeyBusiness = Process(lines, 20, true);
 
         return monkeyBusiness;
     }
 
-    private static long Calculate(string[] lines, int maxRounds, int inspectionWorryLevel)
+    /// <summary>
+    /// What is the level of monkey business after 10000 rounds.
+    /// </summary>
+    /// <param name="lines">Instructions</param>
+    /// <returns>Level of monkey business after 10000 rounds</returns>
+    public static long PartTwo(string[] lines)
+    {
+        var monkeyBusiness = Process(lines, 10000);
+
+        return monkeyBusiness;
+    }
+
+    /// <summary>
+    /// Go over the instructions (lines) and run the process to calculate the monkey business.
+    /// </summary>
+    /// <param name="lines">Instructions</param>
+    /// <param name="maxRounds">Maximum number of rounds.</param>
+    /// <param name="inspectionReduceWorryLevel">Consider a reduction of worry
+    /// level per item per inspection.</param>
+    /// <returns>Monkey business amount.</returns>
+    private static long Process(string[] lines, int maxRounds, bool inspectionReduceWorryLevel = false)
     {
         var monkeys = CreateMonkeys(lines);
 
+        // Modular arithmetic
+        // Required to prevent an overflow on the integer by using least common multiplier (LCM) as the modulo
+        // This is not required for part one
         var lcm = monkeys.Select(x => x.DivisibleBy)
                          .Aggregate(1, (a, b) => a * b);
 
-        var round = 1;
-        while (round <= maxRounds)
+        for (var round = 1; round <= maxRounds; round++)
         {
             for (var i = 0; i < monkeys.Count(); i++)
             {
                 var monkey = monkeys[i];
 
-                while (monkey.Items.Any())
+                while (monkey.WorryLevelPerItem.Any())
                 {
                     monkey.Inspections++;
-                    var itemWorryLevel = monkey.Items.Dequeue();
+                    var itemWorryLevel = monkey.WorryLevelPerItem.Dequeue();
 
                     itemWorryLevel = monkey.WorryFunction(itemWorryLevel);
 
-                    if (inspectionWorryLevel == 3)
-                    {
-                        itemWorryLevel = (int)Math.Floor(itemWorryLevel / (decimal)inspectionWorryLevel);
-                    }
-                    else
-                    {
-                        itemWorryLevel = itemWorryLevel % lcm;
-                    }
+                    itemWorryLevel = UpdateInspectionWorryLevel(inspectionReduceWorryLevel, lcm, itemWorryLevel);
 
                     var throwToMonkey = monkey.ThrowToNext(itemWorryLevel);
-                    monkeys[throwToMonkey].Items.Enqueue(itemWorryLevel);
+                    monkeys[throwToMonkey].WorryLevelPerItem.Enqueue(itemWorryLevel);
                 }
             }
 
 #if DEBUG
             if (round % 1000 == 0 || round == 1 || round == 20)
             {
-                Console.WriteLine($"== After round {round}");
-                Console.WriteLine(ReportInspectionRate(monkeys));
+                Console.WriteLine(ReportInspectionRate(monkeys, round));
             }
 #endif
-            round++;
         }
 
         var monkeyBusiness = CalculateMonkeyBusiness(monkeys);
         return monkeyBusiness;
     }
 
-    private static IList<Monkey> CreateMonkeys(string[] lines)
+    /// <summary>
+    /// Tranform the monkey description lines to monkey objects.
+    /// </summary>
+    /// <param name="monkeyDescriptions">Description of a monkey.</param>
+    /// <returns>Monkeys based on the provided lines.</returns>
+    private static IList<Monkey> CreateMonkeys(string[] monkeyDescriptions)
     {
         var monkeys = new List<Monkey>();
+        // Only 5 lines of the monkey description are relevant
+        var monkeyDescriptionLength = 7;
 
-        for (var i = 1; i < lines.Length; i = i + 7)
+        for (var i = 1; i < monkeyDescriptions.Length; i = i + monkeyDescriptionLength)
         {
-            var part = lines[i..(i + 5)];
-            monkeys.Add(new Monkey(part));
+            // - 2: Skipping the monkey identifier line (first line) and white line at the end
+            var singleMonkeyDescription = monkeyDescriptions[i..(i + monkeyDescriptionLength - 2)];
+            monkeys.Add(new Monkey(singleMonkeyDescription));
         }
 
         return monkeys;
     }
 
-    private static string ReportInspectionRate(IList<Monkey> monkeys)
+    /// <summary>
+    /// Calculate the item worry level per inspection.
+    /// </summary>
+    /// <param name="inspectionReduceWorryLevel">Reduce the worry level by 3</param>
+    /// <param name="lcm">Least common multiplier to reduce the worry level number. Preventing
+    /// overflowing the item worry levels.</param>
+    /// <param name="itemWorryLevel">Current level of the item worry level.</param>
+    /// <returns>New item worry level.</returns>
+    private static long UpdateInspectionWorryLevel(
+        bool inspectionReduceWorryLevel,
+        int lcm,
+        long itemWorryLevel)
     {
+        if (inspectionReduceWorryLevel)
+        {
+            var inspectionReduceWorryLevelBy = 3d;
+            itemWorryLevel = (int)Math.Floor(itemWorryLevel / inspectionReduceWorryLevelBy);
+        }
+
+        return itemWorryLevel % lcm;
+    }
+
+    /// <summary>
+    /// Generate a report of the number of inspected items per monkey. Useful
+    /// for debugging but not required to get to the answer.
+    /// </summary>
+    /// <param name="monkeys">Monkey to report on.</param>
+    /// <returns>A report containing the number of inspected items per monkey</returns>
+    private static string ReportInspectionRate(IList<Monkey> monkeys, int round)
+    {
+        Console.WriteLine($"== After round {round}");
+
         var result = new StringBuilder();
         for (var i = 0; i < monkeys.Count; i++)
         {
@@ -96,8 +146,8 @@ public abstract class DayElevent : IDay<long>
     /// Calculate the monkey business by taking the most two active monkeys
     /// and multiplying their number of inspections.
     /// </summary>
-    /// <param name="monkeys">Applicable monkeys</param>
-    /// <returns>Monkey business amount</returns>
+    /// <param name="monkeys">Applicable monkeys.</param>
+    /// <returns>Monkey business amount.</returns>
     private static long CalculateMonkeyBusiness(IList<Monkey> monkeys)
     {
         var mostActiveMonkeys = monkeys.OrderByDescending(x => x.Inspections)
@@ -106,13 +156,5 @@ public abstract class DayElevent : IDay<long>
                                        .Aggregate(1L, (a, b) => a * b);
 
         return mostActiveMonkeys;
-    }
-    #endregion
-
-    public static long PartTwo(string[] lines)
-    {
-        var monkeyBusiness = Calculate(lines, 10000, 1);
-
-        return monkeyBusiness;
     }
 }
--- src/day-11/Monkey.cs
+++ src/day-11/Monkey.cs
@@ -4,10 +4,10 @@ public class Monkey
     /// Lists your worry level for each item the monkey is currently holding
     /// in the order they will be inspected.
     /// </summary>
-    public Queue<long> Items { get; init; }
+    public Queue<long> WorryLevelPerItem { get; init; }
 
     /// <summary>
-    /// Total number of times by the monkey
+    /// Total number of times by the monkey.
     /// </summary>
     public int Inspections { get; set; }
 
@@ -19,6 +19,9 @@ public class Monkey
     /// </summary>
     public Func<long, long> WorryFunction { get; init; }
 
+    /// <summary>
+    /// Used by ThrowToNext.
+    /// </summary>
     public int DivisibleBy { get; init; }
 
     /// <summary>
@@ -39,70 +42,104 @@ public class Monkey
     /// </summary>
     public int FalseThrowTo { get; init; }
 
+    /// <summary>
+    /// Initialize a monkey based on a string monkey description.
+    /// </summary>
+    /// <param name="monkeyDescription">Description on the monkey.</param>
     public Monkey(string[] monkeyDescription)
     {
-        this.Items = ParseStartingItems(monkeyDescription[0]);
+        this.WorryLevelPerItem = ParseStartingItems(monkeyDescription[0]);
         this.WorryFunction = ParseOperation(monkeyDescription[1]);
         this.DivisibleBy = ParseDivisibleBy(monkeyDescription[2]);
-        this.ThrowToNext = ParseThrowToNextTest(monkeyDescription[2]);
+        this.ThrowToNext = ParseThrowToNextTest();
         this.TrueThrowTo = ParseThrowTo(monkeyDescription[3]);
         this.FalseThrowTo = ParseThrowTo(monkeyDescription[4]);
     }
 
-    private Queue<long> ParseStartingItems(string line)
+    /// <summary>
+    /// Convert the items worry levels to an array.
+    /// Sample input: Starting items: 54, 65, 75, 74
+    /// </summary>
+    /// <param name="itemsWorryLevels">Description of the item worry levels.</param>
+    /// <returns>Worry level per item.</returns>
+    private Queue<long> ParseStartingItems(string itemsWorryLevels)
     {
-        var startingItems = line.Split(':')[1].Trim();
+        var startingItems = itemsWorryLevels.Split(':')[1].Trim();
         return new Queue<long>(startingItems.Split(',')
                             .Select(long.Parse));
     }
 
-    private Func<long, long> ParseOperation(string line)
+    /// <summary>
+    /// Dictionary containing all the supported operators.
+    /// </summary>
+    private static Dictionary<char, Func<long, long, long>> operators = new()
     {
-        var operatorPart = line.Substring(line.IndexOf("old ") + 4, 1)[0];
+        { '+', (x, y) => x + y },
+        { '*', (x, y) => x * y },
+    };
 
-        var operationPart = line.Substring(line.LastIndexOf(' ') + 1);
+    /// <summary>
+    /// Convert the operation line to an worry function.
+    /// Sample input: Operation: new = old * 19
+    ///               Operation: new = old + old
+    /// </summary>
+    /// <param name="operationLine">Description of the operation.</param>
+    /// <returns>Worry function.</returns>
+    private Func<long, long> ParseOperation(string operationLine)
+    {
+        // Retrieve the operator from the operation line
+        // Transform "Operation: new = old * 19" to '*'
+        var operatorPart = operationLine.Substring(operationLine.IndexOf("old ") + 4, 1)[0];
 
-        if (operationPart.Equals("old", StringComparison.InvariantCultureIgnoreCase))
+        // Retrieve the operation part after the operator
+        // Transform "Operation: new = old * 19" to "19"
+        var operationPart = operationLine.Substring(operationLine.LastIndexOf(' ') + 1);
+
+        int amount;
+        // The operation part can be numeric or can hold the value "old"
+        if (int.TryParse(operationPart, out amount))
         {
-            switch (operatorPart)
-            {
-                case '+':
-                    return (worryLevel) => worryLevel + worryLevel;
-                case '*':
-                    return (worryLevel) => worryLevel * worryLevel;
-                default:
-                    throw new NotSupportedException();
-            }
-
+            return (worryLevel) => operators[operatorPart](worryLevel, amount);
         }
 
-        var amount = int.Parse(operationPart);
-
-        switch (operatorPart)
-        {
-            case '+':
-                return (worryLevel) => worryLevel + amount;
-            case '*':
-                return (worryLevel) => worryLevel * amount;
-            default:
-                throw new NotSupportedException();
-        }
+        // Handle situation where the operation part is "old"
+        return (worryLevel) => operators[operatorPart](worryLevel, worryLevel);
     }
 
-    private int ParseDivisibleBy(string line)
+    /// <summary>
+    /// Convert the divisible by line to the divisible by number.
+    /// Sample input: Test: divisible by 19
+    /// </summary>
+    /// <param name="divisibleByLine">Description of the divisible by.</param>
+    /// <returns>Divisible by number.</returns>
+    private int ParseDivisibleBy(string divisibleByLine)
     {
-        return int.Parse(line.Substring(line.LastIndexOf(' ') + 1));
+        // Transform "Test: divisible by 19" to 19
+        return int.Parse(divisibleByLine.Substring(divisibleByLine.LastIndexOf(' ') + 1));
     }
 
-    private Func<long, int> ParseThrowToNextTest(string line)
+    /// <summary>
+    /// Determine if the worry level can be divided by the DivisibleBy property.
+    /// Based on the boolean outcome return the throw to value.
+    /// </summary>
+    /// <returns>Returns the monkey number to throw to</returns>
+    private Func<long, int> ParseThrowToNextTest()
     {
         return (worryLevel) => worryLevel % this.DivisibleBy == 0
             ? this.TrueThrowTo
             : this.FalseThrowTo;
     }
 
-    private int ParseThrowTo(string line)
+    /// <summary>
+    /// Convert the throw to line to the monkey number.
+    /// Sample input: If true: throw to monkey 2
+    ///               If false: throw to monkey 0
+    /// </summary>
+    /// <param name="throwToLine">Description of the throw to action</param>
+    /// <returns>The monkey number to throw to.</returns>
+    private int ParseThrowTo(string throwToLine)
     {
-        return int.Parse(line.Substring(line.LastIndexOf(' ') + 1));
+        // Transform "If true: throw to monkey 2" to 2
+        return int.Parse(throwToLine.Substring(throwToLine.LastIndexOf(' ') + 1));
     }
 }
