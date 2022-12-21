772f8e0ef31eb2dee4620aa71b80feab742a0b9e
Rob Kelder <dev@kelderro.nl>
2022-12-21 22:27:27 +0100

    Introduce GlobalSuppressions to suppress the header StyleCop rules

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,4 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1639:File header should have summary", Justification = "<Pending>")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "<Pending>")]
