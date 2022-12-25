821f2b981d6fcb27c1fae1a29607087d90158b13
Rob Kelder <dev@kelderro.nl>
2022-12-25 04:00:10 +0100

    Setting up the day-14 folder

--- src/day-11/DayElevenTests.cs
+++ src/day-11/DayElevenTests.cs
@@ -3,7 +3,7 @@ namespace Aoc.Year2022.Day11;
 using FluentAssertions;
 using Xunit;
 
-public class DayElevenTests
+public class DayFourteenTests
 {
     private readonly string[] unitTestInput = """
     Monkey 0:
