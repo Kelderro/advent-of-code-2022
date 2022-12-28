34aa0255a47eff01e4b6e269970cd5d6b3b7c473
Rob Kelder <dev@kelderro.nl>
2022-12-28 17:22:25 +0100

    Solved day 21 puzzle

--- src/day-21/DayTwentyOneTests.cs
+++ src/day-21/DayTwentyOneTests.cs
@@ -24,7 +24,7 @@ public class DayTwentyOneTests
     """.Split(Environment.NewLine);
 
     [Fact]
-    public void PartOne_OnExampleTestCase_ReturnFewestStepsRequired()
+    public void PartOne_OnExampleTestCase_ReturnNumberYelledByRootMonkey()
     {
         var result = DayTwentyOne.PartOne(this.unitTestInput);
 
@@ -32,13 +32,149 @@ public class DayTwentyOneTests
     }
 
     [Fact]
-    public void PartOne_OnUsingInputFile_ReturnMonkeyBusinessAmount()
+    public void PartOne_OnUsingInputFile_ReturnNumberYelledByRootMonkey()
     {
         var fileInput = ReadInput(21);
 
         var result = DayTwentyOne.PartOne(fileInput);
 
         result.Should().Be(56490240862410);
+    }
+
+    [Fact]
+    public void PartTwo_OnExampleTestCase_ReturnNumberToYell()
+    {
+        var result = DayTwentyOne.PartTwo(this.unitTestInput);
+
+        result.Should().Be(301);
+    }
+
+    [Fact]
+    public void PartTwo_OnAdditionNumberHumnRight_ReturnHumnNumber()
+    {
+        var input = """
+        root: aaaa + humn
+        aaaa: 3
+        humn: 5
+        """.Split(Environment.NewLine);
+
+        var result = DayTwentyOne.PartTwo(input);
+
+        result.Should().Be(3);
+    }
+
+    [Theory]
+    [InlineData("""
+    root: AAAA + BBBB
+    BBBB: 10
+    AAAA: humn + CCCC
+    CCCC: 3
+    humn: 0
+    """, 7)]
+    [InlineData("""
+    root: AAAA + BBBB
+    BBBB: 10
+    AAAA: CCCC + humn
+    CCCC: 3
+    humn: 0
+    """, 7)]
+    public void PartTwo_OnAdditionNumber_ReturnHumnNumber(
+        string input,
+        int expectedHumn)
+    {
+        var inputLines = input.Split(Environment.NewLine);
+
+        var result = DayTwentyOne.PartTwo(inputLines);
+
+        result.Should().Be(7);
+    }
+
+    [Theory]
+    [InlineData("""
+    root: AAAA + BBBB
+    BBBB: 10
+    AAAA: humn - CCCC
+    CCCC: 2
+    humn: 0
+    """, 12)]
+    [InlineData("""
+    root: AAAA + BBBB
+    BBBB: 10
+    AAAA: CCCC - humn
+    CCCC: 2
+    humn: 0
+    """, -8)]
+    public void PartTwo_OnSubtractionNumber_ReturnHumnNumber(
+        string input,
+        int expectedHumn)
+    {
+        var inputLines = input.Split(Environment.NewLine);
+
+        var result = DayTwentyOne.PartTwo(inputLines);
+
+        result.Should().Be(expectedHumn);
+    }
+
+    [Theory]
+    [InlineData("""
+    root: AAAA + BBBB
+    BBBB: 10
+    AAAA: humn / CCCC
+    CCCC: 2
+    humn: 0
+    """, 20L)]
+    [InlineData("""
+    root: AAAA + BBBB
+    BBBB: 10
+    AAAA: CCCC / humn
+    CCCC: 20
+    humn: 0
+    """, 2L)]
+    public void PartTwo_OnDivisionNumber_ReturnHumnNumber(
+    string input,
+    long expectedHumn)
+    {
+        var inputLines = input.Split(Environment.NewLine);
+
+        var result = DayTwentyOne.PartTwo(inputLines);
+
+        result.Should().Be(expectedHumn);
+    }
+
+    [Theory]
+    [InlineData("""
+    root: AAAA + BBBB
+    BBBB: 10
+    AAAA: humn * CCCC
+    CCCC: 2
+    humn: 0
+    """, 5)]
+    [InlineData("""
+    root: AAAA + BBBB
+    BBBB: 10
+    AAAA: CCCC * humn
+    CCCC: 2
+    humn: 0
+    """, 5)]
+    public void PartTwo_OnMultiplicationNumber_ReturnHumnNumber(
+        string input,
+        int expectedHumn)
+    {
+        var inputLines = input.Split(Environment.NewLine);
+
+        var result = DayTwentyOne.PartTwo(inputLines);
+
+        result.Should().Be(expectedHumn);
+    }
+
+    [Fact]
+    public void PartTwo_OnUsingInputFile_ReturnNumberToYell()
+    {
+        var fileInput = ReadInput(21);
+
+        var result = DayTwentyOne.PartTwo(fileInput);
+
+        result.Should().Be(3403989691757);
     }
 
     private static string[] ReadInput(int dayNumber)
